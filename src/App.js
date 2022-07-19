01630d2948573a09616f55b4355e27ffc5cb77c3
Deep Patel <56002273+deep882001@users.noreply.github.com>
2022-07-19 11:34:20 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Discounts from "./discounts/discounts"
+
+function App() {
+  return (
+    <div>
+    <Discounts/>
+    </div>
+  );
+}
+
+export default App;
